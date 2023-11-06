2ac844d0fcea6e4104c5a49d92703f0ce8d72ff5
Humberto-MS <138087864+Humberto-MS@users.noreply.github.com>
2023-11-05 21:02:04 -0600

    mas cambios del pago (#30)

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ paypal.Buttons ({
   },
   onApprove: (data,actions) => {
     return actions.order.capture().then(function(orderData) {
-      console.log('Capture result', orderData,JSON.stringify(orderData,null,2));
+      //console.log('Capture result', orderData,JSON.stringify(orderData,null,2));
       const transaction = orderData.purchase_units[0].payments.captures[0];
       //alert(`Transaction ${transaction}.status}: ${transaction.id}`);
       boton_paypal.style.display = "none";
