9caee617f6446a6d53757f8394680d6495645823
Humberto-MS <humberto.ms123@gmail.com>
2023-11-20 20:24:05 -0600

    creacion del mapa

--- database.php
+++ database.php
@@ -1,13 +1,13 @@
 <?php
-    // $dbhost = "localhost";
-    // $dbname = "u748726467_cine";
-    // $dbuser = "u748726467_admin";
-    // $dbpass = "pap0Pep0";
-
     $dbhost = "localhost";
-    $dbname = "cine";
-    $dbuser = "root";
-    $dbpass = "";
+    $dbname = "u748726467_cine";
+    $dbuser = "u748726467_admin";
+    $dbpass = "pap0Pep0";
+
+    // $dbhost = "localhost";
+    // $dbname = "cine";
+    // $dbuser = "root";
+    // $dbpass = "";
 
     $conn = mysqli_connect ( $dbhost, $dbuser, $dbpass, $dbname, "3306" ) 
                 or die ( "PROBLEMAS DE CONEXION" );
